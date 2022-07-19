d2575e6b2ecfeecf4176fb322e87c68ba582dcfc
Willian Dantas <williandantas200@gmail.com>
2022-07-19 16:59:24 -0300

    update 19/07/2022

---
+++ js/cidades-atendidas.js
@@ -0,0 +1,6 @@
+let result1 = document.querySelector('.result1')
+let result2 = document.querySelector('.result2')
+
+async function search() {
+// função buscando os CEPS e verificando disponibilidade de cidades atendidas.
+}
